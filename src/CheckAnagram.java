80f59dcb4ab8f07eee839fe41d6086a39d010682
nirmalsharma <nirmal.sharma3500@gmail.com>
2022-09-23 09:07:57 +0530

    Added code of everyday test here

---
+++ src/CheckAnagram.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CheckAnagram {
+    public static void main(String[] args) {
+        String string = "cbaebabacd";
+        String pattern = "abc";
+        List<Integer> list = new ArrayList<>();
+        checkForAnagram(string, pattern, list);
+        System.out.println(list);
+    }
+
+    private static void checkForAnagram(String string, String pattern, List<Integer> list) {
+        for (int i = 0; i < string.length() - pattern.length(); i++) {
+            String substring = string.substring(i, i + pattern.length());
+            int count = 0;
+            for (int j = 0; j < pattern.length(); j++) {
+                int k = j + 1;
+                if (substring.contains(pattern.substring(j, k))) {
+                    count++;
+                }
+            }
+            if (count == pattern.length()) {
+                list.add(i);
+            }
+//            Line 16 to 25 can be replaced by line number 28 to 32, and it will improve speed also,
+//            but it's more like hardcode and not scalable
+//            if (substring.contains(pattern.substring(0, 1))
+//                    && substring.contains(pattern.substring(1, 2))
+//                    && substring.contains(pattern.substring(2, 3))) {
+//                list.add(i);
+//            }
+        }
+    }
+}
---
+++ src/StockSell.java
@@ -0,0 +1,17 @@
+class StockSell {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 10, 11, 3, 9, 15};
+        System.out.println("Final Profit " + makeMaxProfit(array));
+    }
+
+    static int makeMaxProfit(int[] array) {
+        int maxProfit = 0;
+        for (int i = 1; i < array.length; i++)
+            if (array[i] > array[i - 1]) {
+                System.out.print("Total Profit " + maxProfit);
+                System.out.println(" Today's Profit " + (array[i] - array[i - 1]));
+                maxProfit = maxProfit + array[i] - array[i - 1];
+            }
+        return maxProfit;
+    }
+}
