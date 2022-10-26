276351c6d9f690bdf9f8d9f1232e289c330e9a12
nirmalsharma <nirmal.sharma3500@gmail.com>
2022-10-26 21:54:00 +0530

    Test Array

--- src/EveryDayTest/TestMapEntryExitTest.java
+++ src/EveryDayTest/TestMapEntryExitTest.java
@@ -4,6 +4,13 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+/*We are working on a security system for a badged-access room in our company's building.
+Given an ordered list of employees who used their badge to enter or exit the room, write a function that returns two collections:
+1. All employees who didn't use their badge while exiting the room - they recorded an enter without a matching exit. (All employees are required to leave the room before the log ends.)
+2. All employees who didn't use their badge while entering the room - they recorded an exit without a matching enter. (The room is empty when the log begins.)
+Each collection should contain no duplicates, regardless of how many times a given employee matches the criteria for belonging to it.
+*/
+
 public class TestMapEntryExitTest {
     TestMapEntryExit testMapEntryExit = new TestMapEntryExit();
 
