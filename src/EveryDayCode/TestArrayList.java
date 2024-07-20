c1cad531d2ef3bb3d47dbb955e6fc6abdc3a44c1
Nirmal N Sharma <nirmal.sharma3500@gmail.com>
2024-07-20 19:29:16 +0530

    Updated test cases

--- src/EveryDayCode/TestArrayList.java
+++ src/EveryDayCode/TestArrayList.java
@@ -6,8 +6,8 @@ import java.util.Arrays;
 public class TestArrayList {
     public static void main(String[] args) {
         ArrayList<Integer> a = new ArrayList<>(Arrays.asList(1, 2, 3, 4));
-        for (int i = 0; i < a.size(); i++){
-            if(a.contains(1)){
+        for (int i = 0; i < a.size(); i++) {
+            if (a.contains(1)) {
                 a.remove(i);
             }
         }
